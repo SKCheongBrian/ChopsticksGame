07c76ae3eaecdb0b16bb1be8c08e2d533206898b
DESKTOP-EDPC0V3\b_che <skcheongbrian@gmail.com>
2022-08-01 11:28:25 +0800

    Add button component and replace on user sect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccountLoginSectionComponent } from './account-login-section/account-login-section.component';
-import { WelcomeBoxComponent } from './welcome-box/welcome-box.component';
+import { AccountLoginSectionComponent } from './pages/home/account-login-section/account-login-section.component';
+import { WelcomeBoxComponent } from './pages/home/welcome-box/welcome-box.component';
 import { ButtonComponent } from './components/button/button.component';
 import { HomeComponent } from './pages/home/home.component';
 
--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent implements OnInit {
+
+  @Input() title: string;
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
