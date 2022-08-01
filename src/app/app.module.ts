e3b212abf6c877551df507aafa65bc69d50c8b96
DESKTOP-EDPC0V3\b_che <skcheongbrian@gmail.com>
2022-08-01 10:56:52 +0800

    Add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccountLoginSectionComponent } from './account-login-section/account-login-section.component';
 import { WelcomeBoxComponent } from './welcome-box/welcome-box.component';
+import { ButtonComponent } from './components/button/button.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccountLoginSectionComponent,
-    WelcomeBoxComponent
+    WelcomeBoxComponent,
+    ButtonComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
